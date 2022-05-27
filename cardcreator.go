1239356fd7f6738ffad59c702bb2bbe29cd6b288
Giuseppe Carlone <giuseppe.carlone@gmail.com>
2022-05-27 22:31:04 +0200

    added function calls

---
+++ cardcreator.go
@@ -0,0 +1,5 @@
+package main
+
+func createAnotherCard() string {
+	return "Ace of Flower"
+}
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	/* GO is a statically typed language
+	string keyword può essere omessa perché c'è
+	assegnazione diretta (inferred)
+	*/
+	// var card string = "Ace of Spades" // long form
+	// var card = "Ace of Spades"
+	// var card string
+	card := "Ace of Spades" // := to infer
+	card = "Five of Diamond"
+
+	fmt.Println(card)
+
+	anotherCard := newCard() // interred from function return type
+
+	fmt.Println(anotherCard)
+	fmt.Println(createAnotherCard())
+}
+
+func newCard() string {
+	return "Three of Diamond"
 }
