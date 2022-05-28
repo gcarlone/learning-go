96a7ccd748d1b8ad89177bb4e6acb329ce5bb5ab
Giuseppe Carlone <giuseppe.carlone@gmail.com>
2022-05-28 22:54:32 +0200

    event odd exercise

---
+++ even-and-odd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{}
+	for i := 0; i < 10; i++ {
+		numbers = append(numbers, i)
+	}
+
+	for _, n := range numbers {
+		if n%2 != 0 {
+			fmt.Println(n, "is odd")
+		} else {
+			fmt.Println(n, "is even")
+		}
+	}
+}
