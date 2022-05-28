18cab9a9e62149c9a8f92bcc2f0ac1ada6ce55dd
Giuseppe Carlone <giuseppe.carlone@gmail.com>
2022-05-28 10:17:48 +0200

    deck type and functions

---
+++ deck.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+type deck []string
+
+func newDeck() deck {
+	cardSuits := []string{"Space", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	cards := deck{}
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+
+	return cards
+}
+
+// Receiver function is like a method in OOP
+// d is like this or self in OOP languageas
+// d (a receiver argument) in go usually is a
+// single letter, go avoid terms like this or self
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+	fmt.Println()
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func parseString(s string) deck {
+	return deck(strings.Split(s, ","))
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Option #1 - log the error and return a call to newDeck()
+		// Option #2 - log the error and entirely quit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	return parseString(string(bs))
+}
+
+// Multiple return values and slice splitting
+func deal(d deck, handSize int) (deck, deck) {
+	// d[startIndexInclusive:upToNotIncluding]
+	// d[:upToNotIncluding] starts from 0
+	// d[startIndexInclusive:] up to last element
+	// d[:] all the slice
+	return d[:handSize], d[handSize:]
+}
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	/* GO is a statically typed language
 	string keyword può essere omessa perché c'è
@@ -13,19 +11,27 @@ func main() {
 	card := "Ace of Spades" // := to infer
 	card = "Five of Diamond"
 
-	fmt.Println(card)
-
 	anotherCard := newCard() // interred from function return type
 
-	fmt.Println(anotherCard)
-
 	// every eleement in a slice or in an array have to be of the same type
-	//cards := []string{card, anotherCard, createAnotherCard()}
-	cards := desk{card, anotherCard, createAnotherCard()}
+	// cards := []string{card, anotherCard, createAnotherCard()}
+	cards := deck{card, anotherCard, createAnotherCard()}
 
 	// append do not modify existent slice, return a new slice
 	cards = append(cards, "Six of Spades", "Four of Spades")
+	cards.print()
 
+	cards = newDeck()
+	//fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
+
+	cards = newDeckFromFile("my_cards")
+
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	remainingCards.print()
+
+	cards.shuffle()
 	cards.print()
 }
 
