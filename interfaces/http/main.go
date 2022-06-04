25f0d2258eb9af72422f3e014dd96bea26d41c1e
Giuseppe Carlone <giuseppe.carlone@gmail.com>
2022-06-04 18:23:16 +0200

    interfaces refactored http samples

--- interfaces/http/main.go
+++ interfaces/http/main.go
@@ -13,9 +13,19 @@ func main() {
 		os.Exit(1)
 	}
 
+	printResponseBody(resp)
+}
+
+// nella realtà viene fatto in modo più conciso
+// ed elegante con la helper function
+// io.Copy(os.Stdout, resp.Body)
+func printResponseBody(resp *http.Response) {
 	content := make([]byte, 99999)
 	size, err := resp.Body.Read(content)
 	if err != nil {
+		// come da API EOF è l'errore che viene in ogni caso generato
+		// al termine del content body: non è una vera condizione di
+		// errore o bloccante
 		fmt.Println("Error:", err)
 	}
 	fmt.Println("Readed", size, "bytes")
