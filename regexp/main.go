cc64c609b2d047f83f0de3e89df29856f9ab106e
Giuseppe Carlone <giuseppe.carlone@gmail.com>
2022-06-05 16:13:33 +0200

    regexp

--- regexp/main.go
+++ regexp/main.go
@@ -14,25 +14,11 @@ func main() {
 
 	fmt.Println(result)
 
-	fmt.Println(toWeirdCase2("String"))            // => returns "StRiNg"
-	fmt.Println(toWeirdCase2("Weird string case")) // => returns "WeIrD StRiNg CaSe"
+	fmt.Println(toWeirdCase("String"))            // => returns "StRiNg"
+	fmt.Println(toWeirdCase("Weird string case")) // => returns "WeIrD StRiNg CaSe"
 }
 
 func toWeirdCase(s string) string {
-	words := strings.Split(s, " ")
-
-	r := regexp.MustCompile("..?")
-
-	for i, word := range words {
-		words[i] = r.ReplaceAllStringFunc(word, func(s string) string {
-			return strings.ToUpper(s[:1]) + strings.ToLower(s[1:])
-		})
-	}
-
-	return strings.Join(words, " ")
-}
-
-func toWeirdCase2(s string) string {
 	r := regexp.MustCompile("..? ?")
 
 	return r.ReplaceAllStringFunc(s, func(s string) string {
